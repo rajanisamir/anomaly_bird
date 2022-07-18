ef7b85183a1a7b92868729b5db0cb36d9b9d7e95
rajanisamir <rajanisamir@gmail.com>
2022-07-18 16:47:15 -0500

    added cropped run

--- ae_train.py
+++ ae_train.py
@@ -21,6 +21,7 @@ AUDIO_FILES = [
 SAMPLE_RATE = 22050
 NUM_SAMPLES = 22050
 
+CROPPED_MODE = True
 
 def get_arguments():
     parser = argparse.ArgumentParser(
@@ -88,11 +89,11 @@ def main(args):
     args.exp_dir.mkdir(parents=True, exist_ok=True)
 
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-        sample_rate=SAMPLE_RATE, n_fft=1024, hop_length=512, n_mels=64
+        sample_rate=SAMPLE_RATE, n_fft=1024, hop_length=512, n_mels=(128 if CROPPED_MODE else 64)
     )
 
     bad = BirdAudioDataset(
-        AUDIO_FILES, mel_spectrogram, SAMPLE_RATE, NUM_SAMPLES, device
+        AUDIO_FILES, mel_spectrogram, SAMPLE_RATE, NUM_SAMPLES, device, crop_frequencies=CROPPED_MODE
     )
 
     train_data_loader = DataLoader(bad, batch_size=args.batch_size, shuffle=True)
@@ -115,7 +116,7 @@ def main(args):
     else:
         start_epoch = 0
 
-    log_dir = f"theta_run_birds/lr{args.lr}_wd{args.wd}_bs{args.batch_size}_dim10_files6"
+    log_dir = f"theta_run_birds/lr{args.lr}_wd{args.wd}_bs{args.batch_size}_dim10_files6_cropped"
     writer = SummaryWriter(log_dir)
     print(f"Tensorboard logging at {log_dir}")
 
